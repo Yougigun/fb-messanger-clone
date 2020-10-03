4029f7eec368f39618b8a137897340a403e58a21
Yougigun <s14031403@gmail.com>
2020-10-03 20:06:19 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello I am creating FB messenger clone</h1>
+      {/* input field */}
+      {/* button */}
+      {/* messages themselves */}
+    </div>
+
+  )
+}
+
+export default App;
