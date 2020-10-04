afe965b89428fce541c5024136b6644371fe53ed
Yougigun <s14031403@gmail.com>
2020-10-04 13:21:27 +0800

    Deploy to Github page

---
+++ docs/precache-manifest.36e27545eb96a42a7427735dee0786e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b29e8b063653242d002f77fc27c9165",
+    "url": "/index.html"
+  },
+  {
+    "revision": "423255adea12e6b03ba7",
+    "url": "/static/css/main.4f23c2ce.chunk.css"
+  },
+  {
+    "revision": "03431e8f9c6f58a72c6c",
+    "url": "/static/js/2.388ffdd4.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/static/js/2.388ffdd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "423255adea12e6b03ba7",
+    "url": "/static/js/main.16d7f957.chunk.js"
+  },
+  {
+    "revision": "83b7cc9f86aaf60b17f5",
+    "url": "/static/js/runtime-main.9076f898.js"
+  },
+  {
+    "revision": "11b1e8bd1c6a4e34289efdb46a0aa482",
+    "url": "/static/media/Messenger_Logo_Color_RGB.11b1e8bd.png"
+  }
+]);
